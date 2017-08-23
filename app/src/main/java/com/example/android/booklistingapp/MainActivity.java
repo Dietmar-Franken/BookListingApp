ec58111e36b5d3397e24bee8dc475910296a5f1f
thodzic <thodzic@gmail.com>
2017-08-23 06:34:09 -0400

    Trying to clear out the adapter

--- app/src/main/java/com/example/android/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -6,13 +6,16 @@ import android.content.Loader;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.view.inputmethod.InputMethodManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,23 +37,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_list);
 
-        //Find a reference to the ListView in the layout
-        ListView bookListView = (ListView) findViewById(R.id.list);
-
-        // Create a new adapter that takes an empty list of books as imput
-        mAdapter = new BookAdapter(this,new ArrayList<Book>());
-
-        //Set the adapter on the ListView
-        //so the list can be populated in the user interface
-        bookListView.setAdapter(mAdapter);
-
-        //Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        //Get details on the currently active default data network
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
         final Button submitButton = (Button) findViewById(R.id.search_button);
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
@@ -65,18 +51,14 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
                 //so the list can be populated in the user interface
                 bookListView.setAdapter(mAdapter);
 
-                //Get a reference to the ConnectivityManager to check state of network connectivity
-                ConnectivityManager connMgr = (ConnectivityManager)
-                        getSystemService(Context.CONNECTIVITY_SERVICE);
-
-                //Get details on the currently active default data network
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
                 LoaderManager loaderManager = getLoaderManager();
+
 
                 //Initiate the loader.  Pass in the int ID constant defined above and pass in null for
                 //the bundle.  Pass in this activity for the LoaderCAllbacks parameter (which is valid
                 //because this activity implements the LoaderCallbacks interface).
                 loaderManager.initLoader(BOOK_LOADER_ID, null, MainActivity.this);
+
             }
         });
     }
@@ -92,11 +74,11 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
     @Override
     public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
-        EditText search_bar = (EditText) findViewById(R.id.search_bar);
-        search_bar.setVisibility(View.GONE);
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        Button submitButton = (Button) findViewById(R.id.search_button);
-        submitButton.setVisibility(View.GONE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
         //Clear the adapter of previous book data
         mAdapter.clear();
         //If there is a valid list of books, then add them to the adapter's dataset.  This will
@@ -110,5 +92,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     public void onLoaderReset(Loader<List<Book>> loader) {
         //Loader reset, so we can clear out our exisiting data
         mAdapter.clear();
+        mAdapter.notifyDataSetChanged();
     }
 }
